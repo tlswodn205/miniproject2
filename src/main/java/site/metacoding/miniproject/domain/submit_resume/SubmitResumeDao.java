87db3fa58b18f2f91ab8151558141f1cb28104e3
unknown <shyue02@naver.com>
2022-11-02 18:29:08 +0900

    submitResume 완료

--- src/main/java/site/metacoding/miniproject/domain/submit_resume/SubmitResumeDao.java
+++ src/main/java/site/metacoding/miniproject/domain/submit_resume/SubmitResumeDao.java
@@ -2,8 +2,11 @@ package site.metacoding.miniproject.domain.submit_resume;
 
 import java.util.List;
 
+import site.metacoding.miniproject.dto.request.resume.SubmitResumeReqDto;
+import site.metacoding.miniproject.dto.response.resume.SubmitResumeRespDto;
+
 public interface SubmitResumeDao {
-    public void insert(SubmitResume submitResume);
+    public void insert(SubmitResumeReqDto submitResumeReqDto);
 
     public SubmitResume findById(Integer submitResumeId);
 
@@ -12,6 +15,8 @@ public interface SubmitResumeDao {
     public void update(SubmitResume submitResume); // dto생각
 
     public void deleteById(Integer submitResumeId);
-    
+
     public List<SubmitResume> findByNoticeId(Integer noticeId);
+
+    public SubmitResumeRespDto submitResumeResult(SubmitResumeReqDto submitResumeReqDto);
 }
