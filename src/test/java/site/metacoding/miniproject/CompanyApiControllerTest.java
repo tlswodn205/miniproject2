d39561fecedd4f459bd0e3edd331e144170f4322
kim_jiwon <106166409+Kim-Jione@users.noreply.github.com>
2022-11-04 18:45:03 +0900

    kjw-junit미완성테스트 (#12)
    * kjw-junit미완성테스트
    
    * junit 테스트2
    
    * junit 테스트 완료
    
    * junit sql파일 세팅 완료
    
    Co-authored-by: tlswodn205 <80434390+tlswodn205@users.noreply.github.com>

--- src/test/java/site/metacoding/miniproject/CompanyApiControllerTest.java
+++ src/test/java/site/metacoding/miniproject/CompanyApiControllerTest.java
@@ -1,6 +1,8 @@
 package site.metacoding.miniproject;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,16 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import site.metacoding.miniproject.domain.user.User;
 import site.metacoding.miniproject.domain.user.UserDao;
 import site.metacoding.miniproject.dto.SessionUserDto;
@@ -28,65 +34,90 @@ import site.metacoding.miniproject.dto.request.company.CompanyJoinReqDto;
 @SpringBootTest(webEnvironment = WebEnvironment.MOCK) // MOCK은 가짜 환경임
 public class CompanyApiControllerTest {
 
-  private static final String APPLICATION_JSON =
-    "application/json; charset=utf-8";
+    private static final String APPLICATION_JSON = "application/json; charset=utf-8";
 
-  @Autowired
-  private MockMvc mvc; // 이걸로 통신을 한다
+    @Autowired
+    private MockMvc mvc; // 이걸로 통신을 한다
 
-  @Autowired
-  private ObjectMapper om;
+    @Autowired
+    private ObjectMapper om;
 
-  @Autowired
-  private UserDao userDao;
+    @Autowired
+    private UserDao userDao;
 
-  private MockHttpSession session;
+    private MockHttpSession session;
 
-  @BeforeEach
-  public void sessionInit() {
-    session = new MockHttpSession(); // 직접 new를 했다 MockHttpSession해야 Mock가 된다
-    User user = User.builder().userId(1).username("ssar").build(); // password 는 없다
-    session.setAttribute("sessionUserDto", new SessionUserDto(user)); // 가짜세션이 만들어진 상태이다 -> 아직 주입은 안된 상태
-  }
+    // @BeforeEach
+    // public void sessionInit() {
+    //     session = new MockHttpSession();// 직접 new를 했다 MockHttpSession해야 Mock가 된다
+    //     User user = User.builder().userId(1).username("ssar").build();// password 는 없다
+    //     session.setAttribute("sessionUserDto", new SessionUserDto(user));// 가짜세션이 만들어진 상태이다 -> 아직 주입은 안된 상태
+    // }
 
-  @BeforeEach
-  public void dataInit() {
-    User user = User
-      .builder()
-      .userId(1)
-      .username("ssar")
-      .password("1234")
-      .build();
-    int userPS = userDao.save(user);
-  }
+    // @BeforeEach
+    // public void dataInit() {
+    // }
 
-  // @Sql(scripts = "classpath:create.sql", executionPhase =
-  // ExecutionPhase.BEFORE_TEST_METHOD)
-  @Test
-  public void join_test() throws Exception {
-    // given
-    CompanyJoinReqDto companyJoinDto = new CompanyJoinReqDto();
-    companyJoinDto.setUsername("asdfasdf");
-    companyJoinDto.setPassword("1234");
-    companyJoinDto.setRole("company");
-    String body = om.writeValueAsString(companyJoinDto);
+    // 기업회원가입
+    @Sql(scripts = "classpath:create.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Test
+    public void joinCompany_test() throws Exception {
+        // given
+        CompanyJoinReqDto companyJoinDto = new CompanyJoinReqDto();
+        companyJoinDto.setUsername("apttftf");
+        companyJoinDto.setPassword("1234");
+        companyJoinDto.setRole("company");
 
-    // when
-    ResultActions resultActions = mvc.perform(
-      MockMvcRequestBuilders
-        .post("/login")
-        .content(body)
-        .contentType(APPLICATION_JSON)
-        .accept(APPLICATION_JSON)
-    );
-    System.out.println(
-      "디버그 : " + resultActions.andReturn().getResponse().getContentAsString()
-    );
-    // then
-    MvcResult mvcResult = resultActions.andReturn();
-    System.out.println(
-      "디버그 : " + mvcResult.getResponse().getContentAsString()
-    );
-    resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
-  }
+        String body = om.writeValueAsString(companyJoinDto);
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.post("/company/join").content(body)
+                        .contentType(APPLICATION_JSON).accept(APPLICATION_JSON));
+        System.out.println("디버그 : " + resultActions.andReturn().getResponse().getContentAsString());
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(MockMvcResultMatchers.jsonPath("$.code").value(1));
+    }
+
+    // 기업 회원가입 페이지
+    @Test
+    public void companyJoinForm_test() throws Exception {
+
+        // given
+        Integer userId = 1;
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(get("/companyJoinForm").accept(APPLICATION_JSON));
+        System.out.println("디버그 : " + resultActions.andReturn().getResponse().getContentAsString());
+
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$.code").value(1));
+    }
+
+    // 기업추천 리스트 페이지
+    @Test
+    public void recommendListFrom_test() throws Exception {
+
+        // given
+
+        // when
+        ResultActions resultActions = mvc
+                .perform(MockMvcRequestBuilders.get("/company/recommendListFrom")
+                        .accept(APPLICATION_JSON));
+        System.out.println("디버그 : " + resultActions.andReturn().getResponse().getContentAsString());
+
+        // then
+        MvcResult mvcResult = resultActions.andReturn();
+        System.out.println("디버그 : " + mvcResult.getResponse().getContentAsString());
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(jsonPath("$.code").value(1));
+        resultActions.andExpect(jsonPath("$.data.[0].").value("스프링1강"));
+    }
+
 }
